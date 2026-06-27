2725bfdfbce52017a9102d4787a6917d73dd619d
John Franco <franco@unielogics.com>
2026-06-27 18:53:25 -0400

    Fix Bedrock migration revision id

--- alembic/versions/0079_bedrock_ai_usage_default.py
+++ alembic/versions/0079_bedrock_ai_usage_default.py
@@ -1,6 +1,6 @@
 """bedrock ai usage default.
 
-Revision ID: 0079
+Revision ID: 0079_bedrock_ai_usage_default
 Revises: 0078
 Create Date: 2026-06-24
 """
@@ -10,7 +10,7 @@ from __future__ import annotations
 import sqlalchemy as sa
 from alembic import op
 
-revision = "0079"
+revision = "0079_bedrock_ai_usage_default"
 down_revision = "0078"
 branch_labels = None
 depends_on = None
